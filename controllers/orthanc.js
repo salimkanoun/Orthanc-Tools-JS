e77aa77edcf0ea319735a231dc8b44d7d3c4724b
Salim Kanoun <salim.kanoun@gmail.com>
2019-10-14 23:51:41 +0200

    Add files via upload

---
+++ controllers/orthanc.js
@@ -0,0 +1,24 @@
+var Orthanc =require('../model/Orthanc');
+
+var orthancInstance=new Orthanc();
+
+var getResults = function(req, res){
+    orthancInstance.getSystem(function (orthancSystem){
+        console.log(orthancSystem);
+        res.render('index',{title :orthancSystem.DicomPort});
+    });
+
+    
+    orthancInstance.getArchiveDicom(['c7972902-3aa31e0c-ca7b2c78-4aa358d7-8e3f85a0'], function(answer){
+        console.log(answer);
+    });
+    
+
+    orthancInstance.putPeer("pacs","AwServer","localhost",8042,"Generic", function(answer){
+        console.log(answer);
+    })
+
+
+}
+
+module.exports={getResults};
