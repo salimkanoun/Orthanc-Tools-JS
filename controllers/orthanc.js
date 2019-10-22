16964d77ec4191cdfe36a923a033dffb588d097c
Salim Kanoun <salim.kanoun@gmail.com>
2019-10-22 10:42:03 +0200

    small

--- controllers/orthanc.js
+++ controllers/orthanc.js
@@ -4,15 +4,16 @@ let OrthancPatient=require('../model/OrthancPatient');
 var orthancInstance=new Orthanc();
    
 var getResults = async function(req, res){
-    //orthancInstance.makeAnon('studies', '871a6d1f-faa16eeb-9daeae3e-7c78b393-e925978c', 'Default', 'access', 'id', 'name', 'desc');
+    
  
     let aets=await orthancInstance.getAvailableAet();
     console.log(aets);
     let systemInfo=await orthancInstance.getSystem();
     console.log(systemInfo);
-    await orthancInstance.putAet("pacs3","gfdgfdgd","localhost",8042,"Generic");
-    console.log(aets);
+    await orthancInstance.putAet("pacs3","gfdgfdgd","localhost", 8042, "Generic");
     res.render('index', {title : 'Image Fetcher', availableAets : aets });
+
+    //orthancInstance.makeAnon('studies', '871a6d1f-faa16eeb-9daeae3e-7c78b393-e925978c', 'Default', 'access', 'id', 'name', 'desc');
 
     //orthancInstance.deleteFromOrhtanc('studies', 'd0dd07ba-8cc16fb7-84633638-327a8f93-4190a2d6');
 
