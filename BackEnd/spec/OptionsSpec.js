2a264bae237a5941c97e8905612ff3b7c9f6397d
Salim Kanoun <salim.kanoun@gmail.com>
2020-02-08 22:51:45 +0100

    tests

---
+++ BackEnd/spec/OptionsSpec.js
@@ -0,0 +1,20 @@
+const Options = require('../model/Options')
+
+describe('Testing Options', () =>{
+
+    it('should be 22 00 by defaults', async () => {
+
+        let schedule =await Options.getOptions()
+        expect(schedule.hour).toBe(22)
+        expect(schedule.min).toBe(00)
+
+    })
+
+    it('should set schedule', async ()=> {
+        await Options.setScheduleTime(21, 30);
+        let schedule =await Options.getOptions()
+        expect(schedule.hour).toBe(21)
+        expect(schedule.min).toBe(30)
+        await Options.setScheduleTime(22, 00);
+    })
+})
--- BackEnd/spec/UserSpec.js
+++ BackEnd/spec/UserSpec.js
@@ -1,5 +1,21 @@
 const User = require('../model/Users')
 
+// setup console reporter
+const JasmineConsoleReporter = require('jasmine-console-reporter');
+const reporter = new JasmineConsoleReporter({
+    colors: 1,           // (0|false)|(1|true)|2
+    cleanStack: 1,       // (0|false)|(1|true)|2|3
+    verbosity: 4,        // (0|false)|1|2|(3|true)|4|Object
+    listStyle: 'indent', // "flat"|"indent"
+    timeUnit: 'ms',      // "ms"|"ns"|"s"
+    timeThreshold: { ok: 500, warn: 1000, ouch: 3000 }, // Object|Number
+    activity: true,
+    emoji: true,         // boolean or emoji-map object
+    beep: true
+});
+
+jasmine.getEnv().addReporter(reporter);
+
 describe('Testing User Creation', () =>{
 
     it('should create user', async ()=> {
