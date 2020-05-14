9908161c4ad03dc74191909e943947cb2f406f39
Salim Kanoun <salim.kanoun@gmail.com>
2020-05-14 23:32:24 +0200

    export/anon from query

--- FrontEnd/src/services/jobs.js
+++ FrontEnd/src/services/jobs.js
@@ -13,6 +13,22 @@ const jobs = {
       })
     },
 
+    getJobInfos(jobId){
+        return fetch('/api/jobs/' + jobId, {
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+            }
+          }).then((answer) => {
+            if (!answer.ok) { throw answer }
+            return answer.json()
+          }).catch(error => {
+            toastifyError(error)
+          })
+
+    },
+
     cancelJob(jobId){
       return fetch('/api/jobs/' + jobId + '/cancel', {
         method: 'POST',
