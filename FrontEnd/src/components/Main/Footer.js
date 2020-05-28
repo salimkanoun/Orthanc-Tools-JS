04eb715b3cc5c571c718c97e74be8ee10577e327
Salim Kanoun <salim.kanoun@gmail.com>
2020-05-28 17:37:12 +0200

    testing version release

--- FrontEnd/src/components/Main/Footer.js
+++ FrontEnd/src/components/Main/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
   render () {
         return (
         <footer className="page-footer font-small bg-light">
-            <div className="footer-copyright text-center">Orthanc Tools JS: 0.1.1
+            <div className="footer-copyright text-center">Orthanc Tools JS: 0.1.2
             </div>
         </footer>
     )
