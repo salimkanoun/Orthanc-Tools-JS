1ed7307b28be9682c80237070386f28b580aae61
Sylvain berthier <ber.sylv1@gmail.com>
2020-06-04 17:08:01 +0200

    style

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './style.css'
 import App from './App'
+import './style.css'
 import * as serviceWorker from './serviceWorker'
+
 
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
