64d21b2356a79fd2e63604b57bb4ba8fb6c84894
Salim Kanoun <salim.kanoun@gmail.com>
2020-06-09 11:04:24 +0200

    robot ressource selection

--- FrontEnd/src/components/AutoQuery/Connected_Component/Results.js
+++ FrontEnd/src/components/AutoQuery/Connected_Component/Results.js
@@ -30,17 +30,32 @@ class Results extends Component {
         let retrieveArray = []
         
         //If series details have been loaded robot will be defined at series level
-        if (this.props.seriesFiltered.length > 0) {
-            for (let seriesUID of this.props.seriesFiltered) {
+        if (Object.keys(this.props.resultsSeries).length > 0 ) {
+            let seriesUIDArray =[]
+            //If exist filtered item send them, if no filtered item all series items are sent
+            if(this.props.seriesFiltered.length > 0){
+                seriesUIDArray = this.props.seriesFiltered
+            }else{
+                seriesUIDArray = Object.keys(this.props.resultsSeries)
+            }
+            for (let seriesUID of seriesUIDArray) {
                 let seriesObject = this.props.resultsSeries[seriesUID]
                 retrieveArray.push({
                     ...this.props.results[seriesObject['StudyInstanceUID']],
                     ...seriesObject
                 })
             }
-            //Else only use the study results
+        //Else only use the study results
         } else {
-            for (let studyInstanceUID of this.props.studiesFiltered) {
+            
+            let studiesUIDArray = []
+            if(this.props.studiesFiltered.length >0){
+                studiesUIDArray = this.props.studiesFiltered
+            }else{
+                studiesUIDArray = Object.keys(this.props.results)
+            }
+
+            for (let studyInstanceUID of studiesUIDArray) {
                 retrieveArray.push({ ...this.props.results[studyInstanceUID] })
             }
         }
