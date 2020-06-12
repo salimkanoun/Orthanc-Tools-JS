b6b8c7358bd21f0e4799741178df4ca534e90b9c
Sylvain berthier <ber.sylv1@gmail.com>
2020-06-12 09:34:14 +0200

    refactoring

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import {
+  Switch,
+  Route,
+  withRouter, 
   BrowserRouter
 } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 
 import Footer from './components/Main/Footer'
 import NavBar from './components/Main/NavBar'
+import Query from './components/Query/Components/Query'
+import AutoQueryRoot from './components/AutoQuery/Component/AutoQueryRoot'
+import RobotView from './components/AutoQuery/Component/RobotView'
+import Authentication from './components/Authentication'
+import AdminRootPanel from './components/Admin/AdminRootPanel'
+import Import from './components/Import/Import'
+import ContentRootPanel from './components/OrthancContent/ContentRootPanel'
+import ExportPanel from './components/Export/ExportPanel'
+import AnonRootPanel from './components/Anonymize/AnonRootPanel'
 
 //CSS Boostrap
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -43,18 +56,39 @@ class App extends Component {
     this.setAuthPanel = this.setAuthPanel.bind(this)
   }
   
-  setAuthPanel(bool){
+  setAuthPanel(location){
     this.setState({
-      auth: bool
+      auth: location === '/'
     })
   }
+
+  AnimatedSwitch = withRouter(({location}) => (
+    <>
+      <TransitionGroup>
+        <CSSTransition key={location.key} classNames={location.pathname === '/' ? 'auth' : 'slide'} timeout={500} onEnter={() => this.setAuthPanel(location.pathname)}>
+          <Switch location={location}>
+            <Route exact path='/' component={Authentication} />
+            <Route exact path='/import' component={Import} />
+            <Route exact path='/query' component={Query} />
+            <Route exact path='/auto-query' component={AutoQueryRoot} />
+            <Route exact path='/options' component={AdminRootPanel} />
+            <Route exact path='/orthanc-content' component={ContentRootPanel} />
+            <Route exact path='/robot/:username' render = { (props) => <RobotView username={props.match.params.username} /> } />
+            <Route exact path='/export' component={ExportPanel} />
+            <Route exact path='/anonymize' component={AnonRootPanel} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </>
+  ))
 
   render() {
     return (
       <div className={this.state.auth ? 'authentification' : 'app'}>
         <BrowserRouter>
-          <NavBar setAuthPanel={this.setAuthPanel}/>
-          <Footer/>
+          <NavBar setAuthPanel={this.setAuthPanel} hidden={this.state.auth}/>
+          <this.AnimatedSwitch />
+          <Footer hidden={this.state.auth}/>
         </BrowserRouter>
       </div>
     );
--- FrontEnd/src/components/Main/NavBar.js
+++ FrontEnd/src/components/Main/NavBar.js
@@ -1,28 +1,7 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import ToolsPanel from './ToolsPanel'
 import apis from '../../services/apis'
-
-
-import {
-  Switch,
-  Route,
-  withRouter
-} from 'react-router-dom'
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-
-
-import Query from '../Query/Components/Query'
-import AutoQueryRoot from '../AutoQuery/Component/AutoQueryRoot'
-import RobotView from '../AutoQuery/Component/RobotView'
-import Authentication from '../Authentication'
-import AdminRootPanel from '../Admin/AdminRootPanel'
-import Import from '../Import/Import'
-import ContentRootPanel from '../OrthancContent/ContentRootPanel'
-import ExportPanel from '../Export/ExportPanel'
-import AnonRootPanel from '../Anonymize/AnonRootPanel'
-
-
 
 
 export default class NavBar extends Component {
@@ -43,36 +22,11 @@ export default class NavBar extends Component {
       this.setState({ navBackground: backgroundcolor });
     });
   }
-
-  onEnter(location){
-    this.setState({location: location.pathname})
-    location.pathname === '/' ? this.props.setAuthPanel(true) : this.props.setAuthPanel(false)
-  }
-
-  AnimatedSwitch = withRouter(({location}) => (
-    <>
-      <TransitionGroup>
-        <CSSTransition key={location.key} classNames={location.pathname === '/' ? 'auth' : 'slide'} timeout={500} onEnter={() => this.onEnter(location)}>
-          <Switch location={location}>
-            <Route exact path='/' component={Authentication} />
-            <Route exact path='/import' component={Import} />
-            <Route exact path='/query' component={Query} />
-            <Route exact path='/auto-query' component={AutoQueryRoot} />
-            <Route exact path='/options' component={AdminRootPanel} />
-            <Route exact path='/orthanc-content' component={ContentRootPanel} />
-            <Route exact path='/robot/:username' render = { (props) => <RobotView username={props.match.params.username} /> } />
-            <Route exact path='/export' component={ExportPanel} />
-            <Route exact path='/anonymize' component={AnonRootPanel} />
-          </Switch>
-        </CSSTransition>
-      </TransitionGroup>
-    </>
-  ))
+  
 
   render () {
     return (
-      <Fragment>
-        <nav className='navbar navbar-expand-lg mb-5 fixed-top' style={ {backgroundColor : this.state.navBackground} } hidden={this.state.location === '/'}>
+        <nav className='navbar navbar-expand-lg mb-5 fixed-top' style={ {backgroundColor : this.state.navBackground} } hidden={this.props.hidden}>
           <ul className='navbar-nav mr-auto'>
             <li className='nav-item'>
               <Link className='nav-link' to='/orthanc-content'>Orthanc Content</Link>
@@ -95,9 +49,6 @@ export default class NavBar extends Component {
           </ul>
           <ToolsPanel />
         </nav>
-        
-      {<this.AnimatedSwitch />}
-      </Fragment>
       
     )
   }
