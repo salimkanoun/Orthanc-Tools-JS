92daa0d9d0c00b019c42b41761abda9341d90a3b
leocouderc <leo.coud@gmail.com>
2020-07-02 09:18:19 +0200

    robot fix

--- BackEnd/database/models/option.js
+++ BackEnd/database/models/option.js
@@ -4,7 +4,6 @@ module.exports = (sequelize, DataTypes) => {
     hour: DataTypes.INTEGER,
     min: DataTypes.INTEGER,
     ldap: DataTypes.BOOLEAN,
-    localUser: DataTypes.BOOLEAN
   }, {})
   Option.associate = function (models) {
     // associations can be defined here
