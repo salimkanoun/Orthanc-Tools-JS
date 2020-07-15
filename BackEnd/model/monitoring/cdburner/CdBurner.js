1c28eaed4ba522a91a039369995b971ab4cf5afc
leocouderc <leo.coud@gmail.com>
2020-07-15 17:03:54 +0200

    cdburner

--- BackEnd/model/monitoring/cdburner/CdBurner.js
+++ BackEnd/model/monitoring/cdburner/CdBurner.js
@@ -1,4 +1,4 @@
-const Orthanc = require('../Orthanc')
+const Orthanc = require('../../Orthanc')
 
 class CdBurner {
     
@@ -55,28 +55,101 @@ class CdBurner {
         }    
     }
 
-    _makeCDFromPatient(patientID ) {
+    _makeCDFromPatient(newStablePatientID) {
         //Store the Row number where we are going to display progress
-		rownumber = this.table_burning_history.length;
+		let rownumber = this.table_burning_history.length;
         
-        patient = //ToDo
+        let patient = this.orthanc.findInOrthanc('Patient', '', newStablePatientID, '', '', '', '', '')//Obtenir les infos d un patient depuis son patientID
         
-        studies = Orthanc.findInOrthanc('Study', '', patientID, '', '', '', '', '');
+        let studies = this.orthanc.findInOrthanc('Study', '', newStablePatientID, '', '', '', '', '')
 
         if(studies.length === 1) {
-            //newStableStudyID = []
-            //newStableStudyID.add(studies[0].getOrthancId());
-            res = //ToDo : le numero de sutdi d'un patient donnée
-            //makeCD(newStableStudyID);
-            makeCD(res);
-            //continue;
+            newStableStudyID = studies[0].MainDicomTags.StudyID //Recuper la studyID à partie de la studies du patientID
+            makeCD(newStableStudyID);
         }
 
+        let formattedPatientDOB = "N/A"
+        try {
+            let PatientDOB = patient.MainDicomTags.PatientBirthDate //date d'anniversaire du patient
+            formattedPatientDOB = PatientDOB.toLocaleString() //datte d'anniverssaire formaté du patient
+        } catch (err) {
+            console.log(err)
+        }
+
+        let datInfos = [studies.length]
+        let uniqueModalitiesForPrimera = []
+
+        for(i=0; i<studies.length ; i++) {
+            let formattedDateExamen = "N/A";
+            if(studies[i].MainDicomTags.StudyDate !== null) formattedDateExamen = studies[i].MainDicomTags.StudyDate.toLocaleString();
+            let studyDescription = studies[i].MainDicomTags.StudyDescription;	
+            let accessionNumber = studies[i].MainDicomTags.AccessionNumber;
+            
+            let series = studies[i].Series
+            
+            let modalities = []
+            for(u=0; u<series.length ; u++) {
+                modality = series[u].MainDicomTags.Modality;
+			    if(!modalities.contains(modality)) modalities.push(modality);
+            }
+
+            let modalitiesInStudy
+            for(u=0; u<modalities.length ; u++) {
+                modalitiesInStudy = String.join("//", modalities[u]);
+            }
+
+            for(u=0; u<modalities.length ; u++) {
+                if(!uniqueModalitiesForPrimera.contains(modalities[u])) {
+                    uniqueModalitiesForPrimera.push(modalities[u]);
+                }
+            }
+            datInfos[i] = {
+                Name: patient.MainDicomTags.PatientName,
+                getPatientId: patient.MainDicomTags.PatientID ,
+                formattedDateExamen: formattedDateExamen,
+                studyDescription: studyDescription,
+                accessionNumber: accessionNumber,
+                formattedPatientDOB: formattedPatientDOB,
+                modalitiesInStudy: modalitiesInStudy
+            } 
+        }
+
+        modalitiesInStudyPrimera = String.join("//", uniqueModalitiesForPrimera);
+
+        //Update display status
+        //ToDo
+        
+        //Generate the ZIP with Orthanc IDs dicom
+		orthancIds = []
+		for(u=0; u<studies.length ; u++) {
+			orthancIds.push(studies[u].MainDicomTags.StudyID);
+        }
+        let zip = _generateZip(orthancIds);
+
+
+
     }
 
-    _makeCD(patientID ) {
+    _makeCD(newStableStudyID) {
 
     }
+
+    _generateZip(orthancIds) {
+		zip=null;
+		try {
+			datenow = new Date();
+			file = Files.createTempFile("CD_"+dateFormat.format(datenow) , ".zip");
+			file.toFile().deleteOnExit();			
+			zipDownloader = new ExportZip(connexion);
+			zipDownloader.setConvertZipAction(file.toString(), orthancIds, true);
+			//generate ZIP of DICOMs
+			zipDownloader.generateZip(true);
+			zip=zipDownloader.getGeneratedZipFile();
+		} catch (e) {
+			console.log(e)
+		}
+		return zip;
+	}
     
 }    
 
