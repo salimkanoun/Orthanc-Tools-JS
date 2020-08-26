eeb886dbf3646e6a47f97c8e662a0ac0b2839661
Salim Kanoun <salim.kanoun@gmail.com>
2020-08-26 15:37:47 +0200

    controller and route burner

--- BackEnd/controllers/monitoring.js
+++ BackEnd/controllers/monitoring.js
@@ -1,24 +1,8 @@
-var Orthanc_Monitoring = require('../model/monitoring/Orthanc_Monitoring')
-var CdBurner = require('../model/monitoring/cdburner/CdBurner')
+var {cdBurner} = require('../model/monitoring/cdburner/CdBurner')
 
-var test = function() {
-    console.log("TEST0")
-    orthanc_Monitoring = new Orthanc_Monitoring()
-    //console.log("TEST1")
-    //orthanc_Monitoring.makeMonitor()
-    console.log("TEST2")
-    orthanc_Monitoring.getChangeLastLine()
-    console.log("TEST3")
-    orthanc_Monitoring.setChangeLastLine()
-    console.log("TEST4")
-    orthanc_Monitoring.autoSetChangeLastLine()
-    console.log("TEST5")
-    orthanc_Monitoring._parseOutput()
+var startBurner = async function(req, res) {
+    cdBurner.startCDMonitoring()
+    res.end()
 }
 
-var test2 = function() {
-    console.log("TEST0")
-    cdBurner = new CdBurner()
-}    
-
-module.exports = { test,test2 }
+module.exports = { startBurner }
