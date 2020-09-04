dc1511c4f8800b10468b832bb4c590d2ca88b45d
salim <salim.kanoun@gmail.com>
2020-09-04 17:54:49 +0200

    option burner progress

--- FrontEnd/src/components/Admin/CDBurner/BurnerOptions.js
+++ FrontEnd/src/components/Admin/CDBurner/BurnerOptions.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Select from 'react-select'
 import TranscodeSelector from '../../Export/TranscodeSelector'
 
 export default class BurnerOptions extends Component{
@@ -7,6 +8,22 @@ export default class BurnerOptions extends Component{
         super(props)
         this.handleChange = this.handleChange.bind(this)
     }
+
+    manufacturerOptions = [
+        {value : 'epson', label : 'Epson'},
+        {value : 'primera', label : 'Primera'}
+    ]
+
+    levelOptions = [
+        {value: 'study', label : 'Study'},
+        {value: 'patient', label: 'Patient'}
+    ]
+    
+    supportType = [
+        {value : 'auto', label : 'Auto'},
+        {value : 'CD', label : 'CD'},
+        {value : 'DVD', label : 'DVD'}
+    ]
 
     handleChange(event){
         const target = event.target
@@ -19,22 +36,28 @@ export default class BurnerOptions extends Component{
     
     }
 
-    //Reste : Monitoring Level, Burner Manufacturer, Delete after send, Support Type
-
     render(){
         return (
             <div>
-                 <h2 className="card-title">CD/DVD Burner Options</h2>
+                <h2 className="card-title">CD/DVD Burner Options</h2>
                 <label htmlFor="monitoredFolder">Monitored Folder : </label>
-                <input type = 'text'  className="row form-control" name='monitoredFolder' onChange={this.handleChange}/>
+                <input type = 'text'  className="row form-control" name='monitoredFolder' onChange={this.handleChange} placeholder="Example : C:\\myPath\Epson" />
                 <label htmlFor="viewerFolder">Viewer Folder : </label>
-                <input type = 'text'  className="row form-control" name='viewerFolder' onChange={this.handleChange}/>
+                <input type = 'text'  className="row form-control" name='viewerFolder' onChange={this.handleChange} placeholder="Example : C:\\myPath\Viewer" />
                 <label htmlFor="labelPath">Label Path : </label>
-                <input type = 'text'  className="row form-control" name='labelPath' onChange={this.handleChange}/>
+                <input type = 'text'  className="row form-control" name='labelPath' onChange={this.handleChange} placeholder="Example : C:\\myPath\label" />
                 <label htmlFor="transcodeSelector">Transfer Syntax : </label>
                 <TranscodeSelector name='transcodeSelector'/>
-
-                
+                <label htmlFor="manufacturer">Manufacturer : </label>
+                <Select options={this.manufacturerOptions} name="manufacturer"/>
+                <label htmlFor="monitoringLevel">Monitoring Level : </label>
+                <Select options={this.levelOptions} name = "monitoringLevel" />
+                <label htmlFor="supportType">Support Type : </label>
+                <Select options={this.supportType} name = "supportType" />
+                <div className ="form-control" >
+                    <label htmlFor="deleteOriginal">Delete Original : </label>
+                    <input type = "checkbox"  name="deleteOriginal" value="Delete Original Study/Patient"/>
+                </div>
             </div>
         )
     }
