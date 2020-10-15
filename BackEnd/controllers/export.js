2bb631e51a00f541189a9bd5e4e1c45a39b86295
Legrand Matthieu <legmatt0@gmail.com>
2020-10-15 14:42:06 +0200

    Code cleanup

--- BackEnd/controllers/export.js
+++ BackEnd/controllers/export.js
@@ -1,3 +1,7 @@
+//////////////////////////////////////////////////////////////////////
+////////////////////////////// Imports ///////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
 const Orthanc = require("../model/Orthanc")
 const FtpClient = require("basic-ftp")
 const SftpClient = require("ssh2-sftp-client")
@@ -7,32 +11,40 @@ const fs = require("fs")
 const { error } = require("console")
 const tls = require('tls')
 
-// Declaration of clients
-let ftp = new FtpClient.Client()
-let sftp = new SftpClient()
-let orthancInstance = new Orthanc()
 
-const origCreateSecureContext = tls.createSecureContext;
+//////////////////////////////////////////////////////////////////////
+////////////////////////////// Instances /////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
+const ftp = new FtpClient.Client()
+const sftp = new SftpClient()
+const orthancInstance = new Orthanc()
+
+// tls hack to allow ca to be added 
+const origCreateSecureContext = tls.createSecureContext
 tls.createSecureContext = options => {
-  const context = origCreateSecureContext(options);
+  const context = origCreateSecureContext(options)
 
   const pem = fs
     .readFileSync(path.join(__dirname, '..', 'data', 'certificates', 'rootCA.crt'), { encoding: "ascii" })
     .replace(/\r\n/g, "\n");
 
-  const certs = pem.match(/-----BEGIN CERTIFICATE-----\n[\s\S]+?\n-----END CERTIFICATE-----/g);
+  const certs = pem.match(/-----BEGIN CERTIFICATE-----\n[\s\S]+?\n-----END CERTIFICATE-----/g)
 
   if (!certs) {
-    throw new Error(`Could not parse certificate ./rootCA.crt`);
+    throw new Error(`Could not parse certificate ./rootCA.crt`)
   }
 
   certs.forEach(cert => {
-    context.context.addCACert(cert.trim());
-  });
+    context.context.addCACert(cert.trim())
+  })
 
   return context;
 };
+
+//////////////////////////////////////////////////////////////////////
+/////////////// Options TODO : integreate to options /////////////////
+//////////////////////////////////////////////////////////////////////
 
 const ftpConnectionOption = {
     host: 'localhost',
@@ -40,7 +52,7 @@ const ftpConnectionOption = {
     username: 'legrand',
     password: 'Dnmts!',
     targetFolder : '/home/legrand/test',
-    protocol : 'sftp',
+    protocol : 'ftps',
     privateKey : true,
     privateKeyPassPhrase : 'devellopement'
 }
@@ -53,9 +65,56 @@ const webdavConnectionOption = {
     digest: false
 }
 
+//////////////////////////////////////////////////////////////////////
+////////////////////////////// Functions /////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
-//TODO: 
-// Code cleanup
+const sendArchiveFtp  = function(archivePath, archiveName){
+    return ftp.access({
+        host : ftpConnectionOption.host,
+        user : ftpConnectionOption.username,
+        password : ftpConnectionOption.password,
+        secure : ftpConnectionOption.protocol==="ftps" 
+    }).then(async()=>{
+        // Uploading the archive to the server
+        await ftp.uploadFrom(archivePath, ftpConnectionOption.targetFolder + archiveName)
+    }).then(()=>ftp.close())
+    .catch(error=>{
+        ftp.close()
+        throw error
+    })
+}
+
+const sendArchiveSftp  =  function(archivePath, archiveName){
+    //Creatingthe sftp connection
+    let connectioOptions = (ftpConnectionOption.privateKey?
+        {
+            host: ftpConnectionOption.host,
+            port: ftpConnectionOption.port,
+            username: ftpConnectionOption.username,
+            privateKey: fs.readFileSync(path.join(__dirname, '..', 'data', 'private_key', 'id_rsa')),
+            passphrase: ftpConnectionOption.privateKeyPassPhrase
+        }:
+        {
+            host: ftpConnectionOption.host,
+            port: ftpConnectionOption.port,
+            username: ftpConnectionOption.username,
+            password: ftpConnectionOption.password
+        }
+    )
+    
+    return sftp.connect(connectioOptions).then(async ()=>{
+        await sftp.fastPut(archivePath,ftpConnectionOption.targetFolder+archiveName)
+    }).then(()=>sftp.close())
+    .catch(error=>{
+        sftp.close()
+        throw error
+    })
+}
+
+//////////////////////////////////////////////////////////////////////
+////////////////////////////// Controllers ///////////////////////////
+//////////////////////////////////////////////////////////////////////
 
 const exportFtp = async function(req, res){
     
@@ -66,59 +125,25 @@ const exportFtp = async function(req, res){
     let splitedPath =  archivePath.split('/')
     let archiveName = splitedPath[splitedPath.length -1]
 
-    //Checking used protocol
-    if(ftpConnectionOption.protocol==='sftp'){
-        //Creatingthe sftp connection
-        let connectioOptions = (ftpConnectionOption.privateKey?
-            {
-                host: ftpConnectionOption.host,
-                port: ftpConnectionOption.port,
-                username: ftpConnectionOption.username,
-                privateKey: fs.readFileSync(path.join(__dirname, '..', 'data', 'private_key', 'id_rsa')),
-                passphrase: ftpConnectionOption.privateKeyPassPhrase
-            }:
-            {
-                host: ftpConnectionOption.host,
-                port: ftpConnectionOption.port,
-                username: ftpConnectionOption.username,
-                password: ftpConnectionOption.password
-            }
-        )
-        sftp.connect(connectioOptions).then(async ()=>{
-            await sftp.fastPut(archivePath,ftpConnectionOption.targetFolder+archiveName)
-          }).then(()=>{
-            res.json(true)
-            ftp.close()
-        }).catch(error=>{
-            console.error(error)
-            res.status(500).send(error)
-            ftp.close()
-        })
-    }else if(['ftp', 'ftps'].includes(ftpConnectionOption.protocol)){
-        // Creating ftp/ftps with the server  
-        await ftp.access({
-            host : ftpConnectionOption.host,
-            user : ftpConnectionOption.username,
-            password : ftpConnectionOption.password,
-            secure : ftpConnectionOption.protocol==="ftps" 
-        }).then(async()=>{
-            // Uploading the archive to the server
-            await ftp.uploadFrom(archivePath, ftpConnectionOption.targetFolder + archiveName)
-        }).then(()=>{
-            res.json(true)
-            ftp.close()
-        }).catch(error=>{
-            console.error(error)
-            res.status(500).send(error)
-            ftp.close()
-        })
-    }else{
+    //Check for the protocol
+    if (!['sftp', 'ftp', 'ftps'].includes(ftpConnectionOption.protocol)) {
         error  = 'unknown protocol for ftp export'
         console.error(error)
         res.status(500).send(error)
-    }  
-}
+        return;
+    }
 
+    //Send the archive
+    await (ftpConnectionOption.protocol === "sftp"?
+        sendArchiveSftp(archivePath,archiveName):
+        sendArchiveFtp(archivePath,archiveName)
+    ).then(()=>{
+        res.json(true)
+    }).catch(error=>{
+        console.error(error)
+        res.status(500).send(error)
+    })
+}
 
 
 const exportWebDav = async function(req, res){
@@ -142,5 +167,6 @@ const exportWebDav = async function(req, res){
 
     res.json(true)
 }
+
 
 module.exports = {exportFtp, exportWebDav}
