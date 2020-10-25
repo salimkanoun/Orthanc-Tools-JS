aee191e393cf07c44383ed91763d79267f87358f
Salim Kanoun <salim.kanoun@gmail.com>
2020-10-25 23:11:35 +0100

    retry stream saver

--- FrontEnd/src/services/exportDicom.js
+++ FrontEnd/src/services/exportDicom.js
@@ -1,5 +1,5 @@
 import { toastifyError } from './toastify'
-import downloadjs from 'downloadjs'
+import streamSaver from 'streamsaver'
 
 const exportDicom = {
 
@@ -68,14 +68,39 @@ const exportDicom = {
     })
   },
 
-  async downloadZip(jobID) {
+  downloadZip(jobID) {
+    return fetch('/api/jobs/' + jobID + '/archive', {
+        method: 'GET',
+        headers: {
+            Accept: 'application/zip'
+        }
 
-    const url = '/api/jobs/' + jobID + '/archive'
-    var x=new XMLHttpRequest();
-    x.open( "GET", url , true);
-    x.responseType="blob";
-    x.onload= function(e){downloadjs(e.target.response, jobID+".zip", "application/zip");};
-    x.send();
+    }).then((answer) => {
+        if (!answer.ok) throw answer
+        const fileStream = streamSaver.createWriteStream('export.zip')
+
+        const readableStream = answer.body
+
+        // more optimized
+        if (window.WritableStream && readableStream.pipeTo) {
+          return readableStream.pipeTo(fileStream)
+            .then(() => console.log('done writing'))
+        }
+
+        let writer = fileStream.getWriter()
+
+        const reader = answer.body.getReader()
+        const pump = () => reader.read()
+          .then(res => res.done
+            ? writer.close()
+            : writer.write(res.value).then(pump))
+
+        pump()
+
+    }).catch((error) => {
+        throw error
+    })
+
   }
 
 }
