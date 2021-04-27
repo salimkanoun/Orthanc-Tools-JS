269e92a75321cdd66e7c33d3ee37c0f9bfb20bbd
TagZK <julien.davidou@gmail.com>
2021-04-27 15:01:08 +0200

    MyDicom progress

--- FrontEnd/src/components/MyDicom/MyDicom.js
+++ FrontEnd/src/components/MyDicom/MyDicom.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import apis from '../../services/apis'
 import { connect } from 'react-redux'
-//import { SHA1 } from 'crypto-js/sha1'
+import SHA1 from 'crypto-js/sha1'
+import TableStudy from '../CommonComponents/RessourcesDisplay/TableStudy'
 
 class MyDicom extends Component{
   state = {
@@ -34,60 +35,75 @@ class MyDicom extends Component{
       this.setState({
         labels:label_tab
       })
-      console.log(this.state)
     }else{
       throw new Error('Undefined user')
     }
   }
 
   initializeState = async () => {
+    //Fake data set for GUI test : create 2 labels, add them to admin, add them to fake studies
+
+    /*await apis.label.createLabels('test1')
+    await apis.label.createLabels('test2')*/
     await this.getUser()
+/*
+    await apis.userlabel.createUserLabel(this.state.user.id,'test1')
+    await apis.userlabel.createUserLabel(this.state.user.id,'test2')
+    await apis.studylabel.createStudyLabel('ABCDEFG','test1','patient_exp')
+    await apis.studylabel.createStudyLabel('ZEFDG','test2','patient_test')
+    await apis.studylabel.createStudyLabel('ZEFDG','test1','patient_test')*/
     await this.getUserLabels()
+
   }
 
   getStudiesByLabel = async (name) => {
     return await apis.studylabel.getStudiesLabel(name)
   }
-/*
-  _getOrthancStudyID = () => {
-    let hash = SHA1( this.getPatientID() + '|' + this.getStudyInstanceUID() ).toString()
+
+  _getOrthancStudyID = (patientID,studyInstanceUID) => {
+    let hash = SHA1(patientID + "|" + studyInstanceUID).toString()
     return `${hash.substring(0, 8)}-${hash.substring(8, 16)}-${hash.substring(16, 24)}-${hash.substring(24, 32)}-${hash.substring(32, 40)}`
-  }*/
+  }
 
   handleClick = async (e) => {
     var label = e.target.name
     var studies = await this.getStudiesByLabel(label)
-    console.log(studies)
+    var studies_tab = []
 
     for(var i = 0;i<studies.length;i++){
-      //study instance uid to orthanc id ??? (need patientID but how do we do ???)
-      //get informations for a study
+      let study = studies[i]
+      var orthancID = this._getOrthancStudyID(study.patient_id,study.study_instance_uid)
+      //search for infos about this orthancID and save them with this.state.studies ?
+      studies_tab.push('A')
+      //studies_tab.push(await apis.content.getStudiesDetails(orthancID)) 
     }
+
+    this.setState({
+      studies:studies_tab
+    })
+    this.render()
   }
 
 
 
   render = () => {
-//just to test buttons
-    let labels = null
-    if(this.state.labels!=null){
+    var labels  = []
+    if(this.state.labels!=null || this.state.labels===[]){
       labels = this.state.labels
     }
-    else{
-      labels = []
-    }
-    labels.push('pancrea' , 'mic', 'radio', 'fracture')
 
     return (
       <div>
         <div className='jumbotron' name='buttons'>
           <h1>Labels</h1> 
           {labels.map(label => (
-            <button name={label} key={label} type='button' className='btn btn-primary' onClick={this.handleClick}> {label} </button>
+            <button name={label.label_name} key={label.label_name} type='button' className='btn btn-primary' onClick={this.handleClick}> {label.label_name} </button>
           ))}
         </div>
         
         <div className='jumbotron' name='studies'>
+          Use 'React Table' to make this new table and replace 'bootstrap-react', make it in an other component and pass the data with props
+          <TableStudy name='study table' data={this.state.studies} {...this.props} />
         </div>
       </div>
     )
