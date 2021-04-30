fa6b6b018b1369ff6cda5a0150303a499a4ae2dc
Legrand Matthieu <legmatt0@gmail.com>
2021-04-30 10:52:05 +0200

    Putting the historic table into a modal

--- FrontEnd/src/components/Anonymize/AnonHistoric.js
+++ FrontEnd/src/components/Anonymize/AnonHistoric.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import BootstrapTable from 'react-bootstrap-table-next';
-import { toast } from 'react-toastify';
-import { connect } from 'react-redux'
+import {toast} from 'react-toastify';
+import {connect} from 'react-redux'
 import apis from '../../services/apis';
 import AnonymizedResults from './AnonymizedResults';
 import task from '../../services/task';
+import {Modal} from "react-bootstrap";
+import {addStudiesToDeleteList} from "../../actions/DeleteList";
+import {addStudiesToExportList} from "../../actions/ExportList";
 
 class AnonHistoric extends Component {
 
     state = {
-        selectedTask : null,
+        selectedTask: null,
         rows: []
     }
 
@@ -27,7 +30,9 @@ class AnonHistoric extends Component {
         text: 'Show Details',
         formatter: (cell, row, rowIndex, parentComponent) => {
             return (<div className="text-center">
-                <input type="button" className='btn btn-primary' onClick={() => {this.setState({selectedTask: row.id})}} value="Show Result" />
+                <input type="button" className='btn btn-primary' onClick={() => {
+                    this.setState({selectedTask: row.id})
+                }} value="Show Result"/>
             </div>)
         }
     }, {
@@ -37,7 +42,9 @@ class AnonHistoric extends Component {
         formatter: (cell, row, rowIndex, parentComponent) => {
             return (
                 <div className="text-center">
-                    <input type="button" className='btn btn-danger' onClick={() => parentComponent.deleteJobHandler(row.id, parentComponent.refreshHandler)} value="Remove Job" />
+                    <input type="button" className='btn btn-danger'
+                           onClick={() => parentComponent.deleteJobHandler(row.id, parentComponent.refreshHandler)}
+                           value="Remove Job"/>
                 </div>
             )
         }
@@ -62,44 +69,61 @@ class AnonHistoric extends Component {
     }
 
     deleteJobHandler = async (id, refreshHandler) => {
-        try{
+        try {
             await apis.retrieveRobot.deleteRobot(id)
             refreshHandler()
-        }catch(error){
+        } catch (error) {
             toast.error(error.statusText)
         }
     }
 
     refreshHandler = () => {
         apis.task.getTaskOfUser(this.props.username, 'anonymize')
-                .then(async taksIds =>  await Promise.all(taksIds.map(id=>task.getTask(id))))
-                .then((answerData) => {
-            let rows = []
-            answerData.forEach(robotJob => {
-                rows.push({
-                    id: robotJob.id,
-                    name: robotJob.details.projectName,
-                    username: robotJob.creator,
-                    state: robotJob.state,
-                    queriesNb: robotJob.details.items.length
+            .then(async taksIds => await Promise.all(taksIds.map(id => task.getTask(id))))
+            .then((answerData) => {
+                let rows = []
+                answerData.forEach(robotJob => {
+                    rows.push({
+                        id: robotJob.id,
+                        name: robotJob.details.projectName,
+                        username: robotJob.creator,
+                        state: robotJob.state,
+                        queriesNb: robotJob.details.items.length
+                    })
+                });
+
+                this.setState({
+                    rows: rows
                 })
-            });
 
-            this.setState({
-                rows: rows
-            })
-
-        }).catch(error => { toast.error(error.statusText) })
+            }).catch(error => {
+            if (error.statusCode === 404) {
+                toast.error(error.statusMessage);
+            }
+        })
     }
 
     render = () => {
-        let resultTable = (this.state.selectedTask?<AnonymizedResults anonTaskID={this.state.selectedTask}/>:<></>)
-        
+        let resultTable = (this.state.selectedTask ?
+            <AnonymizedResults anonTaskID={this.state.selectedTask}
+                               addStudiesToDeleteList={this.props.addStudiesToDeleteList}
+                               addStudiesToExportList={this.props.addStudiesToExportList}/> :
+            <></>)
+
         return (
             <>
+                <Modal dialogClassName={"big-modal"} show={!!this.state.selectedTask}
+                       onHide={() => this.setState({selectedTask: null})}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Task Results</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {resultTable}
+                    </Modal.Body>
+                </Modal>
                 <h2 className="card-title">Anonimizations : </h2>
-                <BootstrapTable keyField="username" striped={true} data={this.state.rows} columns={this.columns} wrapperClasses='table-responsive' />
-                {resultTable}
+                <BootstrapTable keyField="username" striped={true} data={this.state.rows} columns={this.columns}
+                                wrapperClasses='table-responsive'/>
             </>
         )
     }
@@ -107,8 +131,13 @@ class AnonHistoric extends Component {
 
 const mapStateToProps = (state) => {
     return {
-      username: state.OrthancTools.username
+        username: state.OrthancTools.username
     }
-  }
+}
 
-export default connect(mapStateToProps)(AnonHistoric)
+const mapDispatchToProps = {
+    addStudiesToDeleteList,
+    addStudiesToExportList
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AnonHistoric)
--- FrontEnd/src/components/Anonymize/AnonymizedResults.js
+++ FrontEnd/src/components/Anonymize/AnonymizedResults.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, {Component} from "react"
 
 import TableStudy from "../CommonComponents/RessourcesDisplay/TableStudy"
 import apis from "../../services/apis"
@@ -9,41 +9,48 @@ import MonitorTask from "../../tools/MonitorTask"
 class AnonymizedResults extends Component {
 
     state = {
-        studies : []
+        studies: []
     }
 
     componentDidMount = async () => {
-        if(this.props.anonTaskID){
+        if (this.props.anonTaskID) {
             let anonTask = await task.getTask(this.props.anonTaskID);
-            if(!!anonTask){
+            if (!!anonTask) {
                 this.handleTask(anonTask);
                 if (!["completed", "failed"].includes(anonTask.state)) {
                     this.monitor = new MonitorTask(this.props.anonTaskID, 4000);
                     this.monitor.onUpdate(this.handleTask.bind(this));
-                    this.monitor.onFinish(()=>{});
+                    this.monitor.onFinish(() => {
+                    });
                     this.monitor.startMonitoringJob();
                 }
             }
         }
     }
 
-    componentWillUnmount = ()=>{
-        if(this.monitor)this.monitor.stopMonitoringJob();
+    componentWillUnmount = () => {
+        if (this.monitor) this.monitor.stopMonitoringJob();
     }
 
     handleTask = async anonTask => {
         let studies = []
         for (const item of anonTask.details.items) {
-            if(item.state === "completed"){
-                let study = (await apis.content.getStudiesDetails(item.result));
-                studies.push({
-                    StudyOrthancID: study.ID,
-                    ...study.MainDicomTags,
-                    ...study.PatientMainDicomTags,
-                    AnonymizedFrom: study.AnonymizedFrom,
-                    newStudyDescription: study.MainDicomTags.newStudyDescription ? study.MainDicomTags.newStudyDescription : '',
-                    newAccessionNumber: study.MainDicomTags.newAccessionNumber ? study.MainDicomTags.newAccessionNumber : ''
-                });
+            if (item.state === "completed") {
+                try {
+                    let study = (await apis.content.getStudiesDetails(item.result));
+                    studies.push({
+                        StudyOrthancID: study.ID,
+                        ...study.MainDicomTags,
+                        ...study.PatientMainDicomTags,
+                        AnonymizedFrom: study.AnonymizedFrom,
+                        newStudyDescription: study.MainDicomTags.newStudyDescription ? study.MainDicomTags.newStudyDescription : '',
+                        newAccessionNumber: study.MainDicomTags.newAccessionNumber ? study.MainDicomTags.newAccessionNumber : ''
+                    });
+                } catch (err) {
+                    if (!err.statusCode === 404) {
+                        throw err;
+                    }
+                }
 
             }
         }
@@ -71,11 +78,13 @@ class AnonymizedResults extends Component {
 
     render = () => {
         return (
-            <div className='jumbotron' >
+            <div className='jumbotron'>
                 <h2 className='card-title mb-3'>Anonymized studies</h2>
                 <div className='row'>
                     <div className='col-sm mb-3'>
-                        <button type='button' className="btn btn-warning float-right" onClick={this.emptyAnonymizedList}>Empty List</button>
+                        <button type='button' className="btn btn-warning float-right"
+                                onClick={this.emptyAnonymizedList}>Empty List
+                        </button>
                         <TableStudy
                             data={this.state.studies}
                             hiddenActionBouton={true}
@@ -89,8 +98,9 @@ class AnonymizedResults extends Component {
                     </div>
                 </div>
                 <div className='text-center'>
-                    <button type='button' className='btn btn-primary mr-3' onClick={this.exportList} >To Export List</button>
-                    <button type='button' className='btn btn-danger' onClick={this.deleteList} >To Delete List</button>
+                    <button type='button' className='btn btn-primary mr-3' onClick={this.exportList}>To Export List
+                    </button>
+                    <button type='button' className='btn btn-danger' onClick={this.deleteList}>To Delete List</button>
                 </div>
             </div>
         )
