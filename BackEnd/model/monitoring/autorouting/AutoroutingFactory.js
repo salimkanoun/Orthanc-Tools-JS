97e7e5f354bbf1ce482924c110bf562c31904a5d
Julien <julien.davidou@etu.iut-tlse3.fr>
2021-05-14 10:58:33 +0200

    Autorouting Factory

--- BackEnd/model/monitoring/autorouting/AutoroutingFactory.js
+++ BackEnd/model/monitoring/autorouting/AutoroutingFactory.js
@@ -2,7 +2,23 @@ const Autorouting = require('./Autorouting')
 const Orthanc_Monitoring = require('../Orthanc_Monitoring')
 
 const AutoroutingFactory = (function (){
-
+    var instance;
+ 
+    async function createInstance() {
+        let monitoring =  MonitoringFactory.getInstance();
+        await monitoring.setSettings()
+        let instanceAutorouting = new Autorouting( monitoring )
+        return instanceAutorouting;
+    }
+ 
+    return {
+        getInstance: async function () {
+            if (!instance) {
+                instance = await createInstance();
+            }
+            return instance;
+        }
+    };
 })
 
 const MonitoringFactory =  (function () {
