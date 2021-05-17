0d369d5ccbc8d3adbc46efe968fcc49eaeb6059f
Julien <julien.davidou@etu.iut-tlse3.fr>
2021-05-17 10:24:53 +0200

    save

--- BackEnd/model/monitoring/autorouting/Autorouting.js
+++ BackEnd/model/monitoring/autorouting/Autorouting.js
@@ -1,5 +1,6 @@
 const Orthanc_Monitoring = require ('../Orthanc_Monitoring')
 const Queue = require('promise-queue')
+const Autorouter = require('../../Autorouters')
 
 class Autorouting{
   constructor(monitoring){
@@ -8,6 +9,56 @@ class Autorouting{
     this.monitoringStarted = false
     this.monitorJobs = this.monitorJobs.bind(this)
     this.jobQueue = new Queue(1, Infinity);
+  }
+
+  setSettings = async () => {
+    this.autorouters_series= []
+    this.autorouters_studies = []
+    this.autorouters_patients = []
+    //put on variable all autorouters that are enabled
+    let autorouter = await Autorouter.getAllAutorouters()
+    for(var i = 0 ; i < autorouter.lentgh ; i++){
+      if(autorouter[i].running==true){
+        switch (autorouter[i].target){
+          case 'Patients':
+            this.autorouters_patients.push(autorouter[i])  
+            break
+          case 'Studies':
+            this.autorouters_studies.push(autorouter[i])
+            break
+          case 'Series':
+            this.autorouters_series.push(autorouter[i])
+            break
+          default:
+            break
+        }
+      }
+    }
+  }
+
+  startAutorouting = async () => {
+    if(this.monitoringStarted) return
+    
+    this.setSettings()
+    this.monitoringStarted=true
+    this._makeListener()
+  }
+
+  autoStartIfNeeded = async () => {
+    await this.setSettings()
+    this.startAutorouting()
+  }
+
+  _makeListener = () => {
+    if(this.autorouters_patients){
+
+    }
+    if(this.autorouters_studies){
+
+    }
+    if(this.autorouters_series){
+      
+    }
   }
 
 
