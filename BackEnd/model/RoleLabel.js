f18dc88185ea076a9be871a1d941fd2c2b354b08
Julien <julien.davidou@etu.iut-tlse3.fr>
2021-06-14 09:13:10 +0200

    Comment backend : model

--- BackEnd/model/RoleLabel.js
+++ BackEnd/model/RoleLabel.js
@@ -2,6 +2,12 @@ const RoleLabelRepo = require('../repository/RoleLabel.js')
 const {OTJSConflictException} = require('../Exceptions/OTJSErrors')
 
 class RoleLabel {
+    /**
+     * Create a RoleLabel
+     * @param {String} role_name name of the role
+     * @param {String} label_name name of the label
+     * @returns 
+     */
     static async createRoleLabel(role_name, label_name) {
         const RoleLabel = await RoleLabelRepo.getRoleLabel(role_name, label_name)
 
@@ -13,19 +19,39 @@ class RoleLabel {
         return RoleLabelRepo.create(role_name, label_name)
     }
 
+    /**
+     * RoleLabel to delete
+     * @param {String} role_name name of the role
+     * @param {String} label_name name of the label
+     * @returns 
+     */
     static async deleteRoleLabel(role_name, label_name) {
         return RoleLabelRepo.delete(role_name, label_name)
     }
 
+    /**
+     * Get all RoleLabels
+     * @returns 
+     */
     static async getAll() {
         return RoleLabelRepo.getAllRoleLabel()
     }
 
+    /**
+     * Get all labels linked to one role
+     * @param {String} role_name name of the role
+     * @returns 
+     */
     static async getLabelsFromRoleName(role_name) {
         return RoleLabelRepo.getLabelsByRole(role_name)
     }
 
-    static getRolesFromLabel(label_name) {
+    /**
+     * Get all roles linked to one label
+     * @param {String} label_name name of the label
+     * @returns 
+     */
+    static async getRolesFromLabel(label_name) {
         return RoleLabelRepo.getRolesByLabel(label_name)
     }
 }
