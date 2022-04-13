56c1ec0dc9df70a7328ce8bdb2b9a816577eeb88
delphine <delphine.decap@hotmail.com>
2022-04-13 15:26:07 +0200

    api monitoring burner avec axios

--- FrontEnd/src/services/cdBurner.js
+++ FrontEnd/src/services/cdBurner.js
@@ -1,16 +1,15 @@
+import axios from "axios"
+import { bindActionCreators } from "redux"
+
 const cdBurner = {
 
       startCdBurnerService() {
             let options = {
                   method: 'POST',
-                  headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json; charset=utf-8'
-                  },
                   body: JSON.stringify([])
             }
 
-            return fetch('/api/monitoring/burner/', options).then((answer) => {
+            return axios.post('/api/monitoring/burner/', options).then((answer) => {
                   if (!answer.ok) { throw answer }
                   return (answer.json())
             }).catch(error => {
@@ -22,13 +21,9 @@ const cdBurner = {
 
             let options = {
                   method: 'DELETE',
-                  headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json; charset=utf-8'
-                  }
             }
 
-            return fetch('/api/monitoring/burner/', options).then((answer) => {
+            return axios.delete('/api/monitoring/burner/', options).then((answer) => {
                   if (!answer.ok) { throw answer }
             }).catch((error) => { throw error })
 
@@ -38,12 +33,9 @@ const cdBurner = {
 
             let options = {
                   method: 'GET',
-                  headers: {
-                        Accept: 'application/json'
-                  }
             }
 
-            return fetch('/api/monitoring/burner/', options).then((answer) => {
+            return axios.get('/api/monitoring/burner/', options).then((answer) => {
                   if (!answer.ok) { throw answer }
                   return (answer.json())
             }).catch((error) => {
@@ -56,14 +48,10 @@ const cdBurner = {
 
             let options = {
                   method: 'POST',
-                  headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json; charset=utf-8'
-                  },
                   body: JSON.stringify([])
             }
 
-            return fetch('/api/monitoring/burner/jobs/' + jobBurnerID + '/cancel', options).then((answer) => {
+            return axios.post('/api/monitoring/burner/jobs/' + jobBurnerID + '/cancel', options).then((answer) => {
                   if (!answer.ok) { throw answer }
             }).catch((error) => {
                   throw error
