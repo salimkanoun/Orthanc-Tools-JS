a1c3d4364e30bbc756d771dd5cc9ec993ac603d4
delphine <delphine.decap@hotmail.com>
2022-11-15 08:07:10 +0100

    #714 styliser metadata

--- FrontEnd/src/components/Metadata/MetadataSelector.js
+++ FrontEnd/src/components/Metadata/MetadataSelector.js
@@ -1,33 +1,36 @@
 import React from "react"
-import { Col, Container, FormControl, Row } from "react-bootstrap"
+import { Col, Container, FormControl, Row, Table } from "react-bootstrap"
 import Toggle from 'react-toggle'
 
 export default ({ currentInstanceNumber, numberOfInstances, useSharedTags, onInstanceNumberChange, onSharedTagsChange }) => {
     return (
         <Container fluid>
-            <Col>
-                <label>
-                    <Toggle checked={useSharedTags} onChange={() => onSharedTagsChange()} />
-                    <span> Shared tags </span>
-                </label>
-            </Col>
-            <Col>
-                <Row>
-                    Number of instances : {numberOfInstances}
-                </Row>
-                {
-                    useSharedTags ?
-                        null :
+            <Table>
+                <tr>
+                    <td>
+                        <label>
+                            <Toggle checked={useSharedTags} onChange={() => onSharedTagsChange()} />
+                            <span> Shared tags </span>
+                        </label>
+                    </td>
+                    <td>
                         <Row>
-                            <FormControl type="number" min={1} max={numberOfInstances} value={currentInstanceNumber + 1} onChange={(event) => {
-                                let value = event.target.value
-                                onInstanceNumberChange(Number(value) - 1)
-                            }
-                            } />
+                            Number of instances : {numberOfInstances}
                         </Row>
-                }
-
-            </Col>
+                        {
+                            useSharedTags ?
+                                null :
+                                <Row>
+                                    <FormControl type="number" min={1} max={numberOfInstances} value={currentInstanceNumber + 1} onChange={(event) => {
+                                        let value = event.target.value
+                                        onInstanceNumberChange(Number(value) - 1)
+                                    }
+                                    } />
+                                </Row>
+                        }
+                    </td>
+                </tr>
+            </Table>
         </Container>
     )
 }
