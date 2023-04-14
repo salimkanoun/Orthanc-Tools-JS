b7e70c82a0c98ba1ac5ed0ec01676100fd797c67
Salim Kanoun <salim.kanoun@gmail.com>
2023-04-15 00:07:47 +0200

    starting refac users

--- FrontEnd/src/components/Admin/UserManagement/UserRoot.js
+++ FrontEnd/src/components/Admin/UserManagement/UserRoot.js
@@ -3,43 +3,55 @@ import React, { useState } from 'react'
 import Users from './local/Users'
 import Roles from './roles/Roles'
 import Ldap from './distant/Ldap'
-import { Nav } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
 export default () => {
 
-    const [currentComponent, setCurrentComponent] = useState('Users')
+    const TAB_USERS = 'users'
+    const TAB_ROLES = 'roles'
+    const TAB_LDAP = 'ldap'
+
+    const [currentComponent, setCurrentComponent] = useState(TAB_USERS)
 
     const getComponentToDisplay = () => {
-        let component = null
         switch (currentComponent) {
-            case 'Users':
-                component = <Users />
-                break
-            case 'Roles':
-                component = <Roles />
-                break
-            case 'Ldap':
-                component = <Ldap />
-                break
+            case TAB_USERS:
+                return <Users />
+            case TAB_ROLES:
+                return <Roles />
+            case TAB_LDAP:
+                return <Ldap />
             default:
                 break
         }
-
-        return component
     }
-
-    const switchTab = (tabName) => {
-        setCurrentComponent(tabName)
-    }
-
 
     return (
         <div>
-            <Nav className="mb-5 me-auto" variant="pills" activeKey={currentComponent}>
-                <Nav.Item eventKey={'Users'} onClick={() => switchTab('Users')} >Local Users</Nav.Item>
-                <Nav.Item eventKey={'Roles'} onClick={() => switchTab('Roles')}>Roles</Nav.Item>
-                <Nav.Item eventKey={'Ldap'} onClick={() => switchTab('Ldap')}> Distant Users</Nav.Item>
-            </Nav>
+            <div className='mb-5'>
+                <nav className="otjs-navmenu container-fluid">
+                    <div className="otjs-navmenu-nav">
+                        <li className='col-4 text-center'>
+                            <Button
+                                className={currentComponent === TAB_USERS ? 'otjs-navmenu-nav-link link-button-active link-button' : 'otjs-navmenu-nav-link link-button'}
+                                onClick={() => setCurrentComponent(TAB_USERS)}>Local Users
+                            </Button>
+                        </li>
+                        <li className='col-4 text-center'>
+                            <Button
+                                className={currentComponent === TAB_ROLES ? 'otjs-navmenu-nav-link link-button-active link-button' : 'otjs-navmenu-nav-link link-button'}
+                                onClick={() => setCurrentComponent(TAB_ROLES)}>Roles
+                            </Button>
+                        </li>
+                        <li className='col-4 text-center'>
+                            <Button
+                                className={currentComponent === TAB_LDAP ? 'otjs-navmenu-nav-link link-button-active link-button' : 'otjs-navmenu-nav-link link-button'}
+                                onClick={() => setCurrentComponent(TAB_LDAP)}>Roles
+                            </Button>
+                        </li>
+                    </div>
+                </nav>
+            </div>
             <div>
                 {getComponentToDisplay()}
             </div>
