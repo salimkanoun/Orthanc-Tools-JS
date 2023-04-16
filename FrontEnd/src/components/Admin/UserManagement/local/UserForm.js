20e77f6bf69d860cc2c851825b944312c3cb1d9b
Salim Kanoun <salim.kanoun@gmail.com>
2023-04-16 15:15:07 +0200

    WIP local users

--- FrontEnd/src/components/Admin/UserManagement/local/UserForm.js
+++ FrontEnd/src/components/Admin/UserManagement/local/UserForm.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react"
-import { Button, Form, FormGroup, Modal, Row } from "react-bootstrap"
+import { Form, FormGroup, Row } from "react-bootstrap"
 import SelectRoles from "../SelectRoles"
 
-export default ({ show, setShow, data = {}, modify = false, create = false, queryFunction }) => {
+export default ({ data = {}, children, onValidate }) => {
 
     const [state, setState] = useState({
         username: '',
@@ -15,14 +15,10 @@ export default ({ show, setShow, data = {}, modify = false, create = false, quer
 
     const [role, setRole] = useState('')
 
-    useEffect(()=>{
-        
-        if (modify) {
-            setState({...data})
-            setRole(data.role)
-        }
-
-    }, [])
+    useEffect(() => {
+        setState({ ...data })
+        setRole(data.role)
+    }, [JSON.stringify(data)])
 
 
     const onRolesChange = (event) => {
@@ -37,84 +33,55 @@ export default ({ show, setShow, data = {}, modify = false, create = false, quer
         )
     }
 
-    const resetState = () => {
-        setState({
-            username: '',
-            firstname: '',
-            lastname: '',
-            email: '',
-            password: '',
-            superAdmin: false,
-        })
-        setRole('')
-        setShow(false)
-    }
-
     return (
-        <>
-            <Modal id='create' show={show} onHide={resetState} size='md'>
-                <Modal.Header closeButton>
-                    {
-                        modify ?
-                            <h2 className='card-title'>Modify User</h2>
-                            : null
-                    }
-                    {
-                        create ?
-                            <h2 className='card-title'>Create User</h2>
-                            : null
-                    }
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Row>
-                            <FormGroup>
-                                <Form.Label>Username*</Form.Label>
-                                <Form.Control type='text' placeholder='username' value={state.username} onChange={(event) => onChange('username', event.target.value)} required />
-                            </FormGroup>
-                        </Row>
-                        <Row>
-                            <FormGroup>
-                                <Form.Label>First Name</Form.Label>
-                                <Form.Control type='text' placeholder='First Name' value={state.firstname} onChange={(event) => onChange('firstname', event.target.value)} />
-                            </FormGroup>
-                        </Row>
-                        <Row>
-                            <FormGroup>
-                                <Form.Label>Last Name</Form.Label>
-                                <Form.Control type='text' placeholder='Last Name' value={state.lastname} onChange={(event) => onChange('lastname', event.target.value)} />
-                            </FormGroup>
-                        </Row>
-                        <Row>
-                            <FormGroup>
-                                <Form.Label>Password*</Form.Label>
-                                <Form.Control type='password' placeholder='password' value={state.password} onChange={(event) => onChange('password', event.target.value)} required />
-                            </FormGroup>
-                        </Row>
-                        <Row>
-                            <FormGroup>
-                                <Form.Label>Mail</Form.Label>
-                                <Form.Control type='text' placeholder='example@example.com' value={state.email} onChange={(event) => onChange('email', event.target.value)} />
-                            </FormGroup>
-                        </Row>
-                        <Row>
-                            <FormGroup>
-                                <Form.Label>Super Admin</Form.Label>
-                                <Form.Check Check={state.superAdmin} onChange={(event) => onChange('superAdmin', event.target.value)} />
-                            </FormGroup>
-                        </Row>
-                        <Row>
-                            <FormGroup>
-                                <Form.Label>Roles*</Form.Label>
-                                <SelectRoles onChange={onRolesChange} />
-                            </FormGroup>
-                        </Row>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button className='otjs-button otjs-button-blue' onClick={() => queryFunction(state, role)}>Create</Button>
-                </Modal.Footer>
-            </Modal>
-        </>
+        <Form>
+            <Row>
+                <FormGroup>
+                    <Form.Label>Username*</Form.Label>
+                    <Form.Control type='text' placeholder='username' value={state.username} onChange={(event) => onChange('username', event.target.value)} required />
+                </FormGroup>
+            </Row>
+            <Row>
+                <FormGroup>
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type='text' placeholder='First Name' value={state.firstname} onChange={(event) => onChange('firstname', event.target.value)} />
+                </FormGroup>
+            </Row>
+            <Row>
+                <FormGroup>
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type='text' placeholder='Last Name' value={state.lastname} onChange={(event) => onChange('lastname', event.target.value)} />
+                </FormGroup>
+            </Row>
+            <Row>
+                <FormGroup>
+                    <Form.Label>Password*</Form.Label>
+                    <Form.Control type='password' placeholder='password' value={state.password} onChange={(event) => onChange('password', event.target.value)} required />
+                </FormGroup>
+            </Row>
+            <Row>
+                <FormGroup>
+                    <Form.Label>Mail</Form.Label>
+                    <Form.Control type='text' placeholder='example@example.com' value={state.email} onChange={(event) => onChange('email', event.target.value)} />
+                </FormGroup>
+            </Row>
+            <Row>
+                <FormGroup>
+                    <Form.Label>Super Admin</Form.Label>
+                    <Form.Check Check={state.superAdmin} onChange={(event) => onChange('superAdmin', event.target.value)} />
+                </FormGroup>
+            </Row>
+            <Row>
+                <FormGroup>
+                    <Form.Label>Roles*</Form.Label>
+                    <SelectRoles onChange={onRolesChange} />
+                </FormGroup>
+            </Row>
+            <Row>
+                <div className="d-flex justify-content-end" onClick={() => onValidate({ ...state, role })}>
+                    {children}
+                </div>
+            </Row>
+        </Form>
     )
 }
