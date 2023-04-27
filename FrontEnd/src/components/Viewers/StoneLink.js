73a97552086dfab34815f9504b71343fcdaf8362
Salim Kanoun <salim.kanoun@gmail.com>
2023-04-27 22:47:24 +0200

    add token and uptake ohif link

--- FrontEnd/src/components/Viewers/StoneLink.js
+++ FrontEnd/src/components/Viewers/StoneLink.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { getToken } from '../../services/axios'
 
 /**
  * OHIF Link to open OHIF viewer in another Tab
  * The Backend redirect this destination to OHIF static HTML page integration
  */
 export default ({ StudyInstanceUID, className }) => {
+  const token = getToken()
   return (
     StudyInstanceUID === undefined ?
       null
       :
-      <Link className={className} to={'/viewer-stone/index.html?study=' + StudyInstanceUID} target='_blank'>View in Stone</Link>
+      <Link className={className} to={'/viewer-stone/index.html?study=' + StudyInstanceUID + '&token=' + token} target='_blank'>View in Stone</Link>
   )
 } 
