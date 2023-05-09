91e901625939e0faa0cbb8418a458e1878538ff7
Salim Kanoun <salim.kanoun@gmail.com>
2023-05-09 22:00:49 +0200

    cleaning

--- FrontEnd/src/actions/TableResult.js
+++ FrontEnd/src/actions/TableResult.js
@@ -1,13 +1,13 @@
 import { AQ_REMOVE_STUDY_RESULT, AQ_ADD_STUDY_RESULT, AQ_EMPTY_RESULTS_STUDY_SERIES, AQ_ADD_SERIES_DETAILS, AQ_REMOVE_SERIES_RESULT } from './actions-types'
 
-export function removeResult (lineNumber) {
+export function removeResult(lineNumber) {
   return {
     type: AQ_REMOVE_STUDY_RESULT,
     payload: lineNumber
   }
 }
 
-export function addStudyResult (resultData) {
+export function addStudyResult(resultData) {
   return {
     type: AQ_ADD_STUDY_RESULT,
     payload: {
@@ -16,20 +16,20 @@ export function addStudyResult (resultData) {
   }
 }
 
-export function emptyResultsTable () {
+export function emptyResultsTable() {
   return {
     type: AQ_EMPTY_RESULTS_STUDY_SERIES
   }
 }
 
-export function addSeriesDetails (seriesDetails, studyInstanceUID) {
+export function addSeriesDetails(seriesDetails, studyInstanceUID) {
   return {
     type: AQ_ADD_SERIES_DETAILS,
     payload: { seriesDetails, studyInstanceUID }
   }
 }
 
-export function removeSeriesResult(seriesInstanceUIDArray){
+export function removeSeriesResult(seriesInstanceUIDArray) {
   return {
     type: AQ_REMOVE_SERIES_RESULT,
     payload: seriesInstanceUIDArray
