d68e1f8586b75ba5053feef9c3f6918901e74b99
Salim Kanoun <salim.kanoun@gmail.com>
2023-05-30 21:21:12 +0200

    Query Preset date ne marchent pas #820

--- FrontEnd/src/components/CommonComponents/SearchForm/SearchForm.js
+++ FrontEnd/src/components/CommonComponents/SearchForm/SearchForm.js
@@ -1,18 +1,12 @@
 import React, { useState } from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Container } from 'react-bootstrap'
 import Select from 'react-select'
 import moment from 'moment'
 
 import SelectModalities from './SelectModalities'
 import AetButton from './AetButton'
 
-/**
- * Abstract search form
- * need props :
- * title : title of the form
- * buttons : buttons to validate and treat the form 
- */
-export default ({ icon , title, buttonsName, onFormValidate}) => {
+export default ({ icon, title, buttonsName, onFormValidate }) => {
 
     const [state, setState] = useState({
         firstName: '',
@@ -35,10 +29,6 @@ export default ({ icon , title, buttonsName, onFormValidate}) => {
         { value: 'last3Months', label: 'Last 3 months' },
         { value: 'lastYear', label: 'Last Year' }
     ]
-
-    const getState = () => {
-        return state
-    }
 
     /**
      * Store modality string comming from SelectModalities component in the current state
@@ -103,80 +93,80 @@ export default ({ icon , title, buttonsName, onFormValidate}) => {
             default:
                 setState((state) => ({
                     ...state,
-                    [dateFrom]: '',
-                    [dateTo] : ''
+                    dateFrom: '',
+                    dateTo: ''
                 }))
 
                 break
         }
         setState((state) => ({
             ...state,
-            [dateFrom]: dateFrom,
-            [dateTo] : dateTo
+            dateFrom: dateFrom,
+            dateTo: dateTo
         }))
     }
 
-    //form
-        return (
-            <div>
+    return (
+        <div>
+            <Container fluid>
                 <Row className="border-bottom border-2 pb-3">
                     <Col className="d-flex justify-content-start align-items-center">
                         <i className={icon + " ico me-3"}></i><h2 className="card-title">{title}</h2>
                     </Col>
                 </Row>
-                <div className='row mt-5'>
-                    <div className='col-sm'>
+                <Row className='mt-5'>
+                    <Col sm>
                         <label htmlFor='lastName' className="form-label">Last Name</label>
-                        <input type='text' name='lastName' id='lastName' className='form-control' placeholder='Last name' onChange={handleChange} value={state.lastName} />
-                    </div>
-                    <div className='col-sm'>
+                        <input type='text' name='lastName' className='form-control' placeholder='Last name' onChange={handleChange} value={state.lastName} />
+                    </Col>
+                    <Col sm>
                         <label htmlFor='firstName' className="form-label">First Name</label>
-                        <input type='text' name='firstName' id='firstName' className='form-control' placeholder='First name' onChange={handleChange} value={state.firstName} />
-                    </div>
-                    <div className='col-sm'>
+                        <input type='text' name='firstName' className='form-control' placeholder='First name' onChange={handleChange} value={state.firstName} />
+                    </Col>
+                    <Col sm>
                         <label htmlFor='patientID' className="form-label">Patient ID</label>
-                        <input type='text' name='patientID' id='patientID' className='form-control' placeholder='Patient ID' onChange={handleChange} value={state.patientID} />
-                    </div>
-                </div>
-                <div className='row mt-4'>
-                    <div className='col-sm'>
+                        <input type='text' name='patientID' className='form-control' placeholder='Patient ID' onChange={handleChange} value={state.patientID} />
+                    </Col>
+                </Row>
+                <Row className='mt-4'>
+                    <Col sm>
                         <label htmlFor='accessionNumber' className="form-label">Accession Number</label>
-                        <input type='text' name='accessionNumber' id='accessionNumber' className='form-control' placeholder='Accession Number' onChange={handleChange} value={state.accessionNumber} />
-                    </div>
-                    <div className='col-sm'>
+                        <input type='text' name='accessionNumber' className='form-control' placeholder='Accession Number' onChange={handleChange} value={state.accessionNumber} />
+                    </Col>
+                    <Col sm>
                         <label htmlFor='studyDescription' className="form-label">Study Description</label>
-                        <input type='text' name='studyDescription' id='studyDescription' className='form-control' placeholder='Study Description' onChange={handleChange} value={state.studyDescription} />
-                    </div>
-                    <div className='col-sm'>
+                        <input type='text' name='studyDescription' className='form-control' placeholder='Study Description' onChange={handleChange} value={state.studyDescription} />
+                    </Col>
+                    <Col sm>
                         <label htmlFor='modalities' className="form-label">Modalities</label>
                         <SelectModalities previousModalities={state.modalities} onChange={updateModalities} />
-                    </div>
-                </div>
-                <div className='row mt-4'>
-                    <div className='col-sm'>
+                    </Col>
+                </Row>
+                <Row className='mt-4'>
+                    <Col sm>
                         <label htmlFor='date' className="form-label">Date Preset</label>
                         <Select name="dates" single options={dates} onChange={changeListener} />
-                    </div>
-                    <div className='col-sm'>
+                    </Col>
+                    <Col sm>
                         <label htmlFor='dateFrom' className="form-label">Date From</label>
-                        <input type='date' name='dateFrom' id='dateFrom' className='form-control' placeholder='Date From' onChange={handleChange} value={state.dateFrom} disabled={state.presetDate !== 'none'} />
-                    </div>
-                    <div className='col-sm'>
+                        <input type='date' name='dateFrom' className='form-control' placeholder='Date From' onChange={handleChange} value={state.dateFrom} disabled={state.presetDate !== 'none'} />
+                    </Col>
+                    <Col sm>
                         <label htmlFor='dateTo' className="form-label">Date To</label>
-                        <input type='date' name='dateTo' id='dateTo' className='form-control' placeholder='Date To' onChange={handleChange} value={state.dateTo} disabled={state.presetDate !== 'none'} />
-                    </div>
-                </div>
-                <div className='mt-3 mb-3 text-center'>
-                    <div>
+                        <input type='date' name='dateTo' className='form-control' placeholder='Date To' onChange={handleChange} value={state.dateTo} disabled={state.presetDate !== 'none'} />
+                    </Col>
+                </Row>
+                <Row className='mt-3'>
+                    <div className='d-flex justify-content-center'>
                         {buttonsName.map((aet) => <AetButton
                             key={aet}
                             aetName={aet}
-                            onClick={(event) => { onFormValidate(state, aet) }}
+                            onClick={() => { onFormValidate(state, aet) }}
                         />)}
                     </div>
-                </div>
-
-            </div>
-        )
+                </Row>
+            </Container>
+        </div>
+    )
 
 }
