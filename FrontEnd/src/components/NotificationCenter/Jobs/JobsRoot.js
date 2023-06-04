d8d0fc6712003f029d78aa81f725db73423e89ac
Salim Kanoun <salim.kanoun@gmail.com>
2023-06-04 15:35:23 +0200

    fix refresh notif center Bug affichage si ouvre notification center avant d'ajouter une study retrieve #823

--- FrontEnd/src/components/NotificationCenter/Jobs/JobsRoot.js
+++ FrontEnd/src/components/NotificationCenter/Jobs/JobsRoot.js
@@ -6,32 +6,6 @@ import CardJobs from "../CardJobs";
 import apis from "../../../services/apis";
 
 export default ({ jobNotifications = [], remove }) => {
-  console.log(jobNotifications);
-
-  let pendingJobs = jobNotifications.filter(
-    (notification) =>
-      notification.data?.State !== "Success" &&
-      notification.data?.State !== "Failure"
-  );
-
-  const monitorJobs = async () => {
-
-    for (const notifications of pendingJobs) {
-      let updatedData = JSON.parse(JSON.stringify(notifications.data));
-      const jobId = updatedData.ID;
-      const jobData = await apis.jobs.getJobInfos(jobId);
-      updatedData.State = jobData.State;
-      toast.update(notifications.id, { data: updatedData });
-    }
-  };
-
-  useEffect(() => {
-    monitorJobs();
-    const intervalID = setInterval(monitorJobs, 2000);
-    return () => {
-      clearInterval(intervalID);
-    };
-  }, []);
 
   const clearJobs = () => {
     jobNotifications.map((notification) => {
