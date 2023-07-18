6f3631bd86e5e4495b0c6a37fe60f35944ef506b
salim kanoun <salim.kanoun@gmail.Com>
2023-07-18 13:15:56 +0200

    add buffer

--- FrontEnd/src/components/Content/Preview/InstancePreview.jsx
+++ FrontEnd/src/components/Content/Preview/InstancePreview.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import apis from "../../../services/apis";
 import { errorMessage } from "../../../tools/toastify";
+import { Buffer } from 'buffer/';
 
 export default ({ orthancInstanceID }) => {
   const [imageData, setImageData] = useState(null);
@@ -11,6 +12,7 @@ export default ({ orthancInstanceID }) => {
       .then((data) => {
         let base64ImageString = Buffer.from(data, "binary").toString("base64");
         let srcValue = "data:image/png;base64," + base64ImageString;
+        console.log(srcValue)
         setImageData(srcValue);
       })
       .catch((error) => {console.log(error) ;errorMessage("Preview loading failed")});
--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,7 +1,7 @@
 import svgr from 'vite-plugin-svgr'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
     assetsInclude: ['**/*.md'],
@@ -9,6 +9,20 @@ export default defineConfig({
         svgr(),
         react(),
         //eslint(),
+        nodePolyfills({
+            // To exclude specific polyfills, add them to this list.
+            exclude: [
+              'fs', // Excludes the polyfill for `fs` and `node:fs`.
+            ],
+            // Whether to polyfill specific globals.
+            globals: {
+              Buffer: true, // can also be 'build', 'dev', or false
+              global: true,
+              process: true,
+            },
+            // Whether to polyfill `node:` protocol imports.
+            protocolImports: true,
+          }),
     ],
     server: {
         proxy: {
